9ce9dfd887bd436b0614c37004c6265e33b95c70
Hiromu <hiromu.koumi@gmail.com>
2020-07-28 12:52:46 +0900

    細かな修正

--- app/assets/javascripts/modules/message.js
+++ app/assets/javascripts/modules/message.js
@@ -2,7 +2,7 @@ $(function(){
   function buildHTML(message){
     if ( message.image ) {
       let html =
-        `<div class="message-box">
+        `<div class="message-box" data-message-id=${message.id}>
           <div class="chat-message-info">
             <div class="member-name">
               ${message.user_name}
@@ -21,7 +21,7 @@ $(function(){
       return html;
     } else {
       let html =
-        `<div class="message-box">
+        `<div class="message-box" data-message-id=${message.id}>
           <div class="chat-message-info">
             <div class="member-name">
               ${message.user_name}
@@ -44,15 +44,17 @@ $(function(){
     e.preventDefault()
     let formData = new FormData(this);
     let url = $(this).attr('action');
+    console.log(url)
     $.ajax({
       url: url,
-      type: "POST",
+      type: 'POST',
       data: formData,
       dataType: 'json',
       processData: false,
       contentType: false
     })
     .done(function(data){
+      console.log(data)
       let html = buildHTML(data);
       $('.chat-main__message-list').append(html);
       $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
@@ -61,7 +63,8 @@ $(function(){
     })
     .fail(function(){
       alert("メッセージ送信に失敗しました");
-    })
+      $('.send-button').prop("disabled", false);
+    });
   });
-
+  
 });
